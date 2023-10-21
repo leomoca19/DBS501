f0a6e0a3449ab445d59917763e9040f93afb741c
leomoca19 <leomoca19@gmail.com>
2023-10-21 02:44:58 -0400

    added statements to drop the table and contents of Locations, Departments and Employees

---
+++ dropper.sql
@@ -0,0 +1,17 @@
+-- Drop the contents of the Departments table
+DELETE FROM Departments;
+
+-- Drop the contents of the Employees table
+DELETE FROM Employees;
+
+-- Drop the contents of the Locations table
+DELETE FROM Locations;
+
+-- Drop the Departments table
+DROP TABLE Departments;
+
+-- Drop the Employees table
+DROP TABLE Employees;
+
+-- Drop the Locations table
+DROP TABLE Locations;
